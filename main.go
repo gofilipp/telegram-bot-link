b7ba5a4a238a09c0982a9d6e4c3998bfe48f62ed
gofilipp <phlstrgit@gmail.com>
2026-01-22 21:07:26 +0300

    init app; create a flag handler

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+)
 
+func main() {
+	token := mustToket()
+
+	fmt.Println(token)
+}
+
+func mustToket() string {
+	token := flag.String("t", "", "token for acces to tg bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("broken token")
+	}
+
+	return *token
 }
